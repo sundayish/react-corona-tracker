3f506a13f3ed24f17703410b1eeb4e7fc1a86fb4
CuCue-dev <tm.snntag@icloud.com>
2020-11-13 18:01:45 +0100

    Add: Basic fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit
-            <code>src/App.js</code>
-            and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+import { Cards, Chart, CountryPicker } from "./components";
+import { fetchData } from "./api";
+
+import styles from "./App.module.css";
+
+class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+
+    console.log(data);
+  }
+
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
-  );
+    );
+  }
 }
 
 export default App;
